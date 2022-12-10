1aa33c5893ce780ab51a74653b3b38f0d13f4205
Marcus604 <m.liebermann@me.com>
2022-12-09 23:19:32 -0800

    Add logging and config file

--- Main.py
+++ Main.py
@@ -1,38 +1,101 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+#Libraries
+from configparser import ConfigParser
+import sys
+import RPi.GPIO as GPIO 
 from time import sleep
 import subprocess
 
 
+#Custom
+from log import getLogger, logging
+
+##########CHANGE ME#################
+PROGRAM_NAME = "LG TV Desk Controls"
+
+logger = getLogger(__name__, "logs/{}.log".format(PROGRAM_NAME))
+
+MODE_READ_ONLY = False
+MODE_VERBOSE = False
+
+
+def logLaunch():
+    logger.info("Starting {}".center(40, "=").format(PROGRAM_NAME))
+    if MODE_READ_ONLY:
+        logger.info("Read Only Mode".center(40, "="))
+    if MODE_VERBOSE:
+        logger.info("Verbose Logging".center(40, "="))
+
 def turnOnMac(channel):
-    print("turn on mac!")
+    logger.info("Turning on mac")
     p = subprocess.Popen("lgtv Monitor on", stdout=subprocess.PIPE, shell=True)
     p2 = subprocess.Popen("lgtv Monitor setInput HDMI_1", stdout=subprocess.PIPE, shell=True)
     sleep(0.25)
 
 def turnOnPC(channel):
-    print("turn on pc!")
+    logger.info("Turning on pc")
     p = subprocess.Popen("lgtv Monitor on", stdout=subprocess.PIPE, shell=True)
     p2 = subprocess.Popen("lgtv Monitor setInput HDMI_3", stdout=subprocess.PIPE, shell=True)
     sleep(0.25)
 
 def turnOff(channel):
-    print("turn off!")
+    logger.info("Turning off")
     p = subprocess.Popen("lgtv Monitor off", stdout=subprocess.PIPE, shell=True)
     sleep(0.25)
+    
 
-print("starting!")
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(7,GPIO.RISING,callback=turnOnMac, bouncetime=300) # Setup event on pin 10 rising edge
 
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(10,GPIO.RISING,callback=turnOnPC, bouncetime=300) # Setup event on pin 10 rising edge
+##################################################
+#################Main Application#################
+##################################################
+def main():
+    #Log startup
+    logLaunch()
 
-GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(8,GPIO.RISING,callback=turnOff, bouncetime=300) # Setup event on pin 10 rising edge
+    #Get config file
+    config = ConfigParser()
 
-while True:
-    sleep(0.5)
+    config.read("config.ini")
+    userConfig = config["GPIO_PINS"]
 
-GPIO.cleanup() # Clean up
+    GPIO_PC = int(userConfig["PC_INPUT"])
+    GPIO_LAPTOP = int(userConfig["LAPTOP_INPUT"])
+    GPIO_OFF = int(userConfig["OFF"])
+    
+
+    GPIO.setwarnings(False) 
+    GPIO.setmode(GPIO.BOARD) 
+    GPIO.setup(GPIO_LAPTOP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(GPIO_LAPTOP,GPIO.RISING,callback=turnOnMac, bouncetime=300) 
+
+    GPIO.setup(GPIO_PC, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(GPIO_PC,GPIO.RISING,callback=turnOnPC, bouncetime=300) 
+
+    GPIO.setup(GPIO_OFF, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(GPIO_OFF,GPIO.RISING,callback=turnOff, bouncetime=300) 
+
+    while True:
+        sleep(0.5)
+
+    GPIO.cleanup()
+    
+    logger.info("Finished {}".center(40, "=").format(PROGRAM_NAME))
+
+    
+    
+##################################################
+#####################Launcher#####################
+##################################################
+if __name__ == "__main__":
+
+    VERBOSE_FLAG = "-v"             
+    
+
+    logLevel = logging.INFO
+    if len(sys.argv) != 1:
+        for arg in sys.argv[1:]:
+            if arg == VERBOSE_FLAG:
+                logLevel = logging.DEBUG
+                MODE_VERBOSE = True
+    
+    logger.setLevel(logLevel)
+    main()
